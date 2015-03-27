fcbedd21540c960c88bf7cad6579a0c6fd60a5eb
guilaub <guilaub@guilaub.crans.org>
2015-03-27 20:56:20 +0100

    azerty

--- Aubian.ml
+++ Aubian.ml
@@ -1,2 +1,15 @@
 let parse entree =
-	let (m,n,o,p) 
+	let ic = open_in entree in
+	let (m,n,o,p) = Scanf.scanf "%d %d %d %d " (fun i j k l -> (i,j,k,l)) in
+	let mat = Array.make_matrix m n false in
+	for i=1 to m do
+		let mot = Scanf.fscanf ic "%s " (fun k -> k) in
+			for j=0 to n-1 do
+				mat.(i).(j) <- (mot.[j] = 'H');
+			done;
+	done;
+	(m,n,o,p,mat);;
+
+let test (i,j) a b =
+	let flag = ref true in
+	fo
