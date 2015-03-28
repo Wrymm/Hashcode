6f7c5d3f762a2a79be5d6e61a97c158e86b8419a
Glen Mével <glen.mevel@ens-cachan.fr>
2015-03-28 12:22:17 +0100

    fonction ‘voisins’ bis

--- glen.ml
+++ glen.ml
@@ -4,21 +4,22 @@ let a = 8
 let mat = Array.init r (fun x -> Array.make_matrix c a (1,1))
 
 
-let voisins x y z =
+let voisins (x,y,z) =
   let li = ref [] in
-  let bouger (u,v) =
+  let bouger z' =
+    let (u,v) = mat.(x).(y).(z') in
     let x' = x+u
     and y' = (y+v+c) mod c in
     if 0 <= x' && x' < r then
-      li := (x',y') :: !li
+      li := (x',y',z') :: !li
   in
   (* rester à la même altitude *)
   if z >= 1 then
-    bouger mat.(x).(y).(z);
+    bouger z;
   (* monter *)
   if z < a then
-    bouger mat.(x).(y).(z+1);
+    bouger (z+1);
   (* descendre *)
   if z >= 2 then
-    bouger mat.(x).(y).(z-1);
+    bouger (z-1);
   !li
