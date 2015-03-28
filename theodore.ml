c481b15f712850a6fd3cad6de0ea2cad3372d2a8
Craimefraych <theodore.espagne@gmail.com>
2015-03-28 11:38:36 +0100

    Update theodore.ml

--- theodore.ml
+++ theodore.ml
@@ -1,1 +1,19 @@
-Printf.printf "42\n"
+let r = 100;;
+let c = 300;;
+
+
+let nb_valide l =
+  let t = Array.make_matrix r c 0 in
+  let rec aux = function
+    |[] -> ()
+    |(x,y)::q ->
+	for i = -7 to 7 do
+	  for j = -7 to 7 do
+	    if i*i +j*j <= 49 then
+	      t.(x+i).(y+j) <- t.(x+i).(y+j) + 1;
+	  done
+	done;
+	aux q
+  in
+  aux l;
+  t;;
