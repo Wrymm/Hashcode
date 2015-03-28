7fe6e8b8044c4d157d1d0347d57cf69cd75b6ebf
Craimefraych <theodore.espagne@gmail.com>
2015-03-28 12:11:50 +0100

    Update theodore.ml

--- theodore.ml
+++ theodore.ml
@@ -1,5 +1,6 @@
 let r = 100;;
 let c = 300;;
+let a = 8
 
 
 let nb_valide l =
@@ -10,7 +11,7 @@ let nb_valide l =
 	for i = -7 to 7 do
 	  for j = -7 to 7 do
 	    if i*i +j*j <= 49 then
-	      t.((x+i) mod c).(y+j) <- t.((x+i) mod c).(y+j) + 1;
+	      t.(((x+i)+c) mod c).(y+j) <- t.(((x+i)+c) mod c).(y+j) + 1;
 	  done
 	done;
 	aux q
@@ -29,3 +30,16 @@ let mat_case_valide l =
   in
   aux l;
   t;;
+
+let mat_case_valide_bal bal cibles =
+  let z = mat_case_valide (List.map (fun (x,y,z) -> (x,y)) (Array.to_list bal))in
+  let l = ref [] in
+  let rec aux = function
+    |[] -> []
+    |(x,y)::q -> 
+	if (not z.(x).(y)) then
+	  l := (x,y)::(!l);
+	aux q
+  in
+  mat_case_valide (aux cibles)
+  
