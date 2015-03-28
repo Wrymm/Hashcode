b68d57b682ddb147882baac6886bd637bd1ca59c
Craimefraych <theodore.espagne@gmail.com>
2015-03-28 16:49:28 +0100

    Update theodore.ml

--- theodore.ml
+++ theodore.ml
@@ -124,17 +124,17 @@ let print_val r c  t sortie =
 let chemin p x2 y2 dx dy = List.rev (parcours_largeur p x2 y2 dx dy)
 
 
-let delta = 80;;
+let delta = 10;;
 
 let h d = (d*delta) + delta /2
 
 
-let chemin_mid2 t d = chemin t (h d) 210 delta 0
-let chemin_australie t d = chemin t (h d) 260 (delta) 0
-let chemin_mid t d = chemin t (h d) 10 (delta) 0
-let chemin_amerique t d = chemin t (h d) 100 (delta) 0
-let chemin_afrique t d = chemin t (h d) 167 (delta) 0
-let premier_chemin d = if d < 2 then chemin (1,(rs,cs,1)) (h 0) 210 (delta*3) 0
+let chemin_mid2 t d = chemin t (h d) 210 delta delta
+let chemin_australie t d = chemin t (h d) 260 (delta) delta
+let chemin_mid t d = chemin t (h d) 10 (delta) delta
+let chemin_amerique t d = chemin t (h d) 100 (delta) delta
+let chemin_afrique t d = chemin t (h d) 167 (delta) delta
+let premier_chemin d = if d < 2 then chemin (1,(rs,cs,1)) (h 0) 210 (delta*3) delta
     else
   if d = 5 then chemin (1,(rs,cs,1)) (h 5) 210 (delta*2) 0
   else chemin (1,(rs,cs,1)) (h d) 210 (delta) 0
